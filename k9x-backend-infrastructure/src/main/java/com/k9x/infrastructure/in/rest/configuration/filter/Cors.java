1d0ee2ecb00ef4cef526998c4d189604f3fd91ce
txomin <txomin.sirera@clarity.ai>
2026-07-09 12:02:22 +0200

    verbose

--- k9x-backend-infrastructure/src/main/java/com/k9x/infrastructure/in/rest/configuration/filter/Cors.java
+++ k9x-backend-infrastructure/src/main/java/com/k9x/infrastructure/in/rest/configuration/filter/Cors.java
@@ -17,7 +17,7 @@ public class Cors implements Filter {
     private static final Set<String> ALLOWED_ORIGINS = Set.of(
             "http://localhost:4000",
             "http://localhost:5173",
-            "https://txomin55.github.io/k9x-frontend"
+            "https://txomin55.github.io"
     );
 
     @Override
